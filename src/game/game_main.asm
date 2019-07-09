acf18c004edd2210f7c9beb6772c0bbe2608fdcf
QFSW <yusuf.ismail1024@gmail.com>
2019-07-09 16:18:49 +0100

    Added grid

--- src/game/game_main.asm
+++ src/game/game_main.asm
@@ -1,8 +1,12 @@
 PUBLIC game_main
 EXTERN print:PROC, strlen:PROC
 
+GRID_WIDTH EQU 4
+GRID_SIZE EQU GRID_WIDTH * GRID_WIDTH
+
 .data
 	intro db "Welcome to 2048!", 0
+	grid dq GRID_SIZE DUP(0)
 
 .code
 game_main PROC
