dc843c1378815022767550ac0c12448bc05fb613
QFSW <yusuf.ismail1024@gmail.com>
2019-07-10 19:10:22 +0100

    Finished mergenum

--- src/game/grid/merge/mergenum.asm
+++ src/game/grid/merge/mergenum.asm
@@ -4,19 +4,33 @@ PUBLIC mergenum
 ; RCX = row ptr
 ; RDX = index
 mergenum PROC
-	MOV R8, 0
 	MOV R9, [RCX + RDX * 8]
+	CMP R9, 0
+	JE skp
+	MOV R8, RDX
+	SUB RDX, 1
 mloop:
-	MOV RAX, [RCX + R8 * 8]
+	MOV RAX, [RCX + RDX * 8]
 	CMP RAX, 0
-	JE mmove
-	INC R8
-	CMP R8, RDX
-	JL mloop
+	JE move
+	CMP RAX, R9
+	JE merge
+mloopr:
+	DEC RDX
+	CMP RDX, 0
+	JGE mloop
 	RET
-mmove:
-	MOV [RCX + R8 * 8], R9
-	MOV [RCX + RDX * 8], RAX
+move:
+	MOV [RCX + RDX * 8], R9
+	MOV [RCX + R8 * 8], RAX
+	MOV R8, RDX
+	JMP mloopr
+merge:
+	MOV RAX, 0
+	ADD [RCX + RDX * 8], R9
+	MOV [RCX + R8 * 8], RAX
+	RET
+skp:
 	RET
 mergenum ENDP
 
