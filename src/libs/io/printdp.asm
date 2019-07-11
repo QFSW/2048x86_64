bcb0e8be4dbaf1eaf74d48e1320c7564e517d763
QFSW <yusuf.ismail1024@gmail.com>
2019-07-11 13:28:43 +0100

    Documented and optimised printdp

--- src/libs/io/printdp.asm
+++ src/libs/io/printdp.asm
@@ -6,25 +6,26 @@ EXTERN logn:PROC
 ASCII_SPACE EQU 32
 
 .code
+; prints a number in decimal format with n digits of padding
 ; RCX = num
 ; RDX = digits
 printdp PROC
 	PUSH R12
 	PUSH R13
 	ENTER 32, 0
-	MOV R12, RCX
-	MOV R13, RDX
+	MOV R12, RCX ; num
+	MOV R13, RDX ; digits
 
 	CALL printd
 	MOV RCX, R12
 	MOV RDX, 10
-	CALL logn
 
+	CALL logn
 	SUB R13, RAX
+
 charloop:
 	DEC R13
-	CMP R13, 0
-	JLE chardone
+	JZ chardone
 	MOV RCX, ASCII_SPACE
 	CALL putchar
 	JMP charloop
