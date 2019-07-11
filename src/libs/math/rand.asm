585088e594d45ff2571fce3f089da0ea38f7bccf
QFSW <yusuf.ismail1024@gmail.com>
2019-07-11 13:06:28 +0100

    Documented rand, rseed and rseedt

--- src/libs/math/rand.asm
+++ src/libs/math/rand.asm
@@ -4,18 +4,21 @@ PUBLIC rand, rseed, rseedt
 	rnum dq 3749823
 
 .code
+; seeds the random number generator
 ; RCX = seed
 rseed PROC
 	MOV rnum, RCX
 	RET
 rseed ENDP
 
+; auto seeds the random number generator with time
 rseedt PROC
 	RDTSC
 	MOV rnum, RAX
 	RET
 rseedt ENDP
 
+; gets a random number
 ; RAX = num
 rand PROC
 	ROR rnum, 17
