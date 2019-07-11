e1fd988c574327b18adcc4098ca1879580f6410b
QFSW <yusuf.ismail1024@gmail.com>
2019-07-11 01:21:37 +0100

    Fixed issue#2 (numbers can jump over another number when merging)

--- src/game/grid/merge/mergenum.asm
+++ src/game/grid/merge/mergenum.asm
@@ -11,7 +11,9 @@ mergenum PROC
 	JE skp
 
 	PUSH R12
+	PUSH R13
 	MOV R12, 0
+	MOV R13, 0 ; block check
 	MOV R8, RDX
 	SUB RDX, 1
 mloop:
@@ -20,11 +22,13 @@ mloop:
 	JE move
 	CMP RAX, R9
 	JE merge
+	MOV R13, 1
 mloopr:
 	DEC RDX
 	CMP RDX, 0
 	JGE mloop
 	MOV RAX, R12
+	POP R13
 	POP R12
 	RET
 move:
@@ -34,10 +38,13 @@ move:
 	OR R12, 1
 	JMP mloopr
 merge:
+	CMP R13, 0
+	JNE mloopr
 	MOV RAX, 0
 	ADD [RCX + RDX * 8], R9
 	MOV [RCX + R8 * 8], RAX
 	MOV RAX, 3
+	POP R13
 	POP R12
 	RET
 skp:
