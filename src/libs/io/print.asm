e488624dc656a7fb9bab6149c85eae9c8bc3dd5b
QFSW <yusuf.ismail1024@gmail.com>
2019-07-11 13:17:11 +0100

    Optimised and documented print

--- src/libs/io/print.asm
+++ src/libs/io/print.asm
@@ -2,30 +2,23 @@ PUBLIC print
 EXTERN putchar:PROC
 
 .code
+; prints a string
 ; RCX = str
 print PROC
 	PUSH R12
-	PUSH R13
-	PUSH R14
 	ENTER 32, 0
-
-	MOV R12, RCX
-	MOV R13, 0
+	MOV R12, RCX ; str
 
 ploop:
-	MOV RCX, 0
-	MOV CL, [R12 + R13]
-	MOV R14, RCX
-	CMP R14, 0
+	MOV CL, [R12]
+	CMP CL, 0
 	JE pdone
 	CALL putchar
-	INC R13
+	INC R12
 	JMP ploop
 
 pdone:
 	LEAVE
-	POP R14
-	POP R13
 	POP R12
 	RET
 print ENDP
